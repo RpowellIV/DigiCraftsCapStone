8ee81eba358a20f14c9b5ee03315e4d6b54488b6
Robert <r.powelliv@gmail.com>
2020-12-30 16:12:40 -0500

    first commit

--- tunechamp/src/components/login.jsx
+++ tunechamp/src/components/login.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './login.css'
-import { loginUrl } from './spotify.js'
+import { loginUrl } from './spotify.jsx'
+import TuneChamp from "../media/tuneChampImg.png"
 
 let Login = () => {
     return (
         <div className="login">
             {/* Logo */}
-            <img src="./img/tuneChampImg.png" alt="TuneChamp test logo"></img>
+            <img src={TuneChamp} width='10%' alt="TuneChamp test logo"></img>
             {/* Login w/ spotify button, calls login URL from spotify.js */}
             <button><a href={loginUrl}>LOGIN WITH SPOTIFY</a></button>
         </div>
