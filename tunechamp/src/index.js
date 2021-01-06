6cdaf3995143eed7047ec883d66f7329589123f6
Robert <r.powelliv@gmail.com>
2021-01-05 20:11:25 -0500

    update

--- tunechamp/src/index.js
+++ tunechamp/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
-import Header from './components/header'
-import Login from './components/login'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import './index.css';
+// import store from './redux/store'
 
-let PageFrame = () => { 
+import App from './App';
 
-    return (
-        <div>
-            <Header />
-            <Login />
-        </div>
-    )
-}; 
 
-ReactDOM.render(<PageFrame />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        {/* <Provider store={store}> */}
+            <Router>
+                <App />
+            </Router>
+        {/* </Provider> */}
+    </React.StrictMode>,
+    document.getElementById('root')
+);
