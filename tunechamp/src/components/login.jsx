fc46d86419f618480b1b9a27bdcb07d2a2fc7fdf
LewRit <lewrit16@gmail.com>
2021-01-08 23:03:00 -0500

    fixed style folders

--- tunechamp/src/components/login.jsx
+++ tunechamp/src/components/login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import TuneChamp from "../media/tuneChampImg.png";
-import { Link, Route, Switch } from 'react-router-dom';
 import { Header } from './index';
 import LinkButton from './linkButton'
+import '../styles/login.css'
 
 let Login = () => {
     return (
