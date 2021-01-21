1c46094ac59f62fc396c8d1f410eaf28909291f9
Stephen-Roach <stephenroach21@gmail.com>
2021-01-20 20:45:31 -0600

    fixed leaderboard

--- tunechamp/src/components/login.jsx
+++ tunechamp/src/components/login.jsx
@@ -1,92 +1,93 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
-import LinkButton from './LinkButton'
-import '../styles/Login.css'
-import { loginUrl } from '../spotify'
+import axios from 'axios';
+import LinkButton from './LinkButton';
+import '../styles/Login.css';
+import { loginUrl } from '../spotify';
 import { getTokenFromUrl } from '../spotify';
-import SpotifyWebApi from "spotify-web-api-js";
-import { useDataLayerValue } from "../DataLayer";
-import Playlist from './Playlist'
-
+import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from '../DataLayer';
+import Playlist from './Playlist';
 
 const spotify = new SpotifyWebApi();
 
 function Login() {
   // const [token, setToken] = useState(null);
-    const[{ user, token }, dispatch] = useDataLayerValue();
+  const [{ user, token }, dispatch] = useDataLayerValue();
   // Runs code based on a given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
     //removes token info from url
-    window.location.hash = "";
+    window.location.hash = '';
 
     //pulls token to push through useState
     const _token = hash.access_token;
 
-
-  if (_token) {
-    dispatch({
-      type: 'SET_TOKEN',
-      token: _token,
-    });
-
-    // setToken(_token)
-    spotify.setAccessToken(_token);
-    //Gets all spotify info for logged in person
-    spotify.getMe().then((user) => {
+    if (_token) {
       dispatch({
-        type: 'SET_USER',
-        user: user,
+        type: 'SET_TOKEN',
+        token: _token,
       });
-    });
 
-      spotify.searchTracks('Love') 
-        .then((random) =>{
-            console.log('track>>>', random)
-
-            dispatch({
-                type: 'SET_RANDOM',
-                random: random
-              })
-
+      axios.get('http://localhost:3001/leaderboard').then((response) => {
+        dispatch({
+          type: 'SET_LEADERBOARD',
+          leaderboard: response.data.allUsers,
         });
+      });
+
+      // setToken(_token)
+      spotify.setAccessToken(_token);
+      //Gets all spotify info for logged in person
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+
+      spotify.searchTracks('Love').then((random) => {
+        console.log('track>>>', random);
+
+        dispatch({
+          type: 'SET_RANDOM',
+          random: random,
+        });
+      });
 
       spotify.getUserPlaylists().then((playlists) => {
         dispatch({
           type: 'SET_PLAYLISTS',
-          playlists: playlists
-        })
-      })
+          playlists: playlists,
+        });
+      });
     }
   }, []);
 
-
-  console.log(`user >>>`, user);
-  console.log(`token >>>`, token);
-
-    const sendUser = async () => {
-        const res = await axios.post(
-            'http://localhost:3001/user', 
-            {user:user, token:token})
-        console.log(res);
+  const sendUser = async () => {
+    console.log(user);
+    if (user) {
+      const res = await axios.post('http://localhost:3001/user', {
+        user: user,
+        token: token,
+      });
+      console.log(res);
     }
-
-    sendUser();
-  
-
-// let Login = () => {
-    return (
-            <div className="login">
-                <div className="jumbotron jumbotron-fluid">
-                    <div className="container">
-                        <h1 className="display-4">TUNE CHAMP TEST</h1>
-                        <p className="lead">THE RIGHT GAME FOR GAME NIGHT...</p>
-                    </div>
-                </div>
-                {token ? (<h1>TOKEN</h1>) : (<a href={loginUrl}>LOGIN WITH SPOTIFY TEST</a>)}
-                {/* <a href={loginUrl}>LOGIN WITH SPOTIFY TEST</a> */}
-                {/* <LinkButton to='/home'> ------------</LinkButton> */}
-            </div>
-    )
   };
+
+  sendUser();
+
+  return (
+    <div className='login'>
+      <div className='jumbotron jumbotron-fluid'>
+        <div className='container'>
+          <h1 className='display-4'>TUNE CHAMP TEST</h1>
+          <p className='lead'>THE RIGHT GAME FOR GAME NIGHT...</p>
+        </div>
+      </div>
+      {token ? <h1>TOKEN</h1> : <a href={loginUrl}>LOGIN WITH SPOTIFY TEST</a>}
+      {/* <a href={loginUrl}>LOGIN WITH SPOTIFY TEST</a> */}
+      {/* <LinkButton to='/home'> ------------</LinkButton> */}
+    </div>
+  );
+}
 export default Login;
--- tunechamp/src/reducer.js
+++ tunechamp/src/reducer.js
@@ -1,58 +1,65 @@
 export const initialState = {
-    user:null,
-    playslists: [],
-    random:null,
-    playing: false,
-    item: null,
-    //remove after development
-    token: null
+  user: null,
+  playslists: [],
+  random: null,
+  playing: false,
+  item: null,
+  //remove after development
+  token: null,
+  leaderboard: null,
 };
 
 const reducer = (state, action) => {
-    console.log(action);
+  console.log(action);
 
-    // Action -> type, [payload]
+  // Action -> type, [payload]
 
-    switch(action.type) {
-        case 'SET_USER':
-            return {
-                ...state,
-                user: action.user
-            };
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
 
-        case 'SET_TOKEN':
-            return {
-                ...state,
-                token: action.token
-            };
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      };
 
-        case 'SET_PLAYLISTS':
-            return {
-                ...state,
-                playlists: action.playlists
-            };
+    case 'SET_PLAYLISTS':
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
 
-        case 'SET_RANDOM':
-            return {
-                ...state,
-                random: action.random
-            };
+    case 'SET_RANDOM':
+      return {
+        ...state,
+        random: action.random,
+      };
 
-        case 'SET_PLAYING':
-            return {
-                ...state,
-                playing: action.playing
-            };
+    case 'SET_PLAYING':
+      return {
+        ...state,
+        playing: action.playing,
+      };
 
-            case 'SET_ITEM':
-        return {
-            ...state,
-            item: action.item
-        };
-        
-        default:
-            return state;
-    }
-}
+    case 'SET_ITEM':
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case 'SET_LEADERBOARD':
+      return {
+        ...state,
+        leaderboard: action.leaderboard,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
