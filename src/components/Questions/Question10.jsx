7ee7217f4a8525596e96dab50422a929128555d5
Robert Powell <r.powelliv@gmail.com>
2021-01-23 12:27:00 -0500

    Question Fix

--- src/components/Questions/Question10.jsx
+++ src/components/Questions/Question10.jsx
@@ -1,4 +1,4 @@
-import React, {useState}  from 'react';
+import React, {useState, useEffect}  from 'react';
 import { useDataLayerValue } from '../../DataLayer'
 import SongRow2 from '../SongRow2'
 
